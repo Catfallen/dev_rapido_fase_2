21dbea023b5a0cd53e74a2d3c3d1174b2be3c3a2
Marcos Alexandre <118125964+Catfallen@users.noreply.github.com>
2025-11-13 03:44:44 -0300

    Delete saque directory

--- saque/teste.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-try {
-  fs.accessSync('./producao-843114-b_de_bet.p12', fs.constants.R_OK);
-  console.log('✅ Certificado encontrado!');
-} catch (err) {
-  console.error('❌ Certificado não encontrado:', err);
-}
