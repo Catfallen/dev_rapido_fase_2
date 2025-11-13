79edfa25ab040dc7fe313459b52beaba5061a584
Catfallen <markimprogramador2006@gmail.com>
2025-11-13 04:04:47 -0300

    public routes

--- webhook/app.js
+++ webhook/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const http = require('http');
 const {initWebSocket} = require('./websocket/crash.socket');
 const app = express();
-
+const cors = require('cors');
+const path = require('path');
 
 // ✅ Middleware JSON unificado com rawBody preservado
 app.use(express.json({
@@ -13,18 +14,23 @@ app.use(express.json({
     }
 }));
 
+app.use(cors());
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const paymentRoutes = require('./routes/payment.routes'); //rotas de pagamentos
 const authRoutes = require('./routes/authRoutes'); //routas de autenticação
 const betRoutes = require('./routes/bet.routes'); //rota dos jogos
-
+const publicRoutes = require('./routes/public.routes');
 
 
 app.use('/auth',authRoutes,betRoutes);
 app.use("/payments", paymentRoutes);
 app.use("/bet",betRoutes)
-
+app.use('/public',publicRoutes);
 //teste do websocket 
 const server = http.createServer(app);
 initWebSocket(server);
