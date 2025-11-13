81b1892e2fa7866d25ff95b5060d5c25c16c23d6
Marcos Alexandre <118125964+Catfallen@users.noreply.github.com>
2025-11-13 03:45:48 -0300

    Delete queryss directory

--- queryss/user.sql
+++
@@ -1,9 +0,0 @@
--- =========================================
--- TABELA DE USUÁRIOS
--- =========================================
-CREATE TABLE usuario (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
-);
